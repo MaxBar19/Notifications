f7ce1874c35d66cb70cfdfde1271b014c2bb6e7b
Kovalenko Max <justspook42@gmail.com>
2020-01-23 12:12:56 +0200

    Added list maker and dropdown maker with no styles

--- main.js
+++ main.js
@@ -1,5 +1,31 @@
+const data = ["АА5555ВА", "АА5588ВА", "АА5555НА"]
+let option = '';
+
+
+function makeDropdownFromData(data){
+  for (var i=0;i<data.length;i++){
+    option += '<option value="'+ data[i] + '">' + data[i] + '</option>';
+  }
+  $('#dropdownData').append(option);
+}
+
+function makeListFromData(transport, repeat, duration){
+  $('#transportData').append('<div class="notificationValue"></div>')
+
+  $('.notificationValue').append('<div class="transportValue">' + transport +'</div>')
+  $('.notificationValue').append('<div class="repeatValue">'+ repeat +'</div>')
+  $('.notificationValue').append('<div class="durationValue">' + duration + '</div>')
+  $('.notificationValue').append('<button class="removeData">X</button>')
+
+}
+
+
+
 
 $(document).ready(function () {
+    
+  makeDropdownFromData(data);
+
   $(".checkbox").click(function(){
     $(this).toggleClass('checked')
   });
@@ -10,4 +36,13 @@ $(document).ready(function () {
   $("input.cmn-toggle-round:checked + label:after").click(function(){
     $(this).style("margin-left: 25px;background-color: #44b6f6;");
   })
+
+  $('#addTransport').click(function(){
+    let transport = $('#dropdownData').val();
+    let repeat = $('#inputText').val();
+    let duration = $('#duration').val();
+    
+    makeListFromData(transport, repeat, duration);
+  })
+
 });
